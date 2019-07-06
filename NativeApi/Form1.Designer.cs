f3b4bc7d612b0febeabf6d2d8f737c317aff9688
rokmaid <rokmaid@hotmail.com>
2019-07-06 00:42:28 -0300

    Add project files.

---
+++ NativeApi/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace NativeApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.btnSendToHost = new System.Windows.Forms.Button();
+            this.btnSendToEmulator = new System.Windows.Forms.Button();
+            this.btnGetToken = new System.Windows.Forms.Button();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(53, 71);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedHorizontal;
+            this.txtResult.Size = new System.Drawing.Size(278, 333);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.Text = "";
+            // 
+            // btnSendToHost
+            // 
+            this.btnSendToHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendToHost.Location = new System.Drawing.Point(372, 31);
+            this.btnSendToHost.Name = "btnSendToHost";
+            this.btnSendToHost.Size = new System.Drawing.Size(164, 49);
+            this.btnSendToHost.TabIndex = 1;
+            this.btnSendToHost.Text = "Send to Host";
+            this.btnSendToHost.UseVisualStyleBackColor = true;
+            this.btnSendToHost.Click += new System.EventHandler(this.btnSendToHost_Click);
+            // 
+            // btnSendToEmulator
+            // 
+            this.btnSendToEmulator.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendToEmulator.Location = new System.Drawing.Point(372, 104);
+            this.btnSendToEmulator.Name = "btnSendToEmulator";
+            this.btnSendToEmulator.Size = new System.Drawing.Size(164, 48);
+            this.btnSendToEmulator.TabIndex = 2;
+            this.btnSendToEmulator.Text = "Send to Emulator";
+            this.btnSendToEmulator.UseVisualStyleBackColor = true;
+            this.btnSendToEmulator.Click += new System.EventHandler(this.btnSendToEmulator_Click);
+            // 
+            // btnGetToken
+            // 
+            this.btnGetToken.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetToken.Location = new System.Drawing.Point(372, 176);
+            this.btnGetToken.Name = "btnGetToken";
+            this.btnGetToken.Size = new System.Drawing.Size(164, 51);
+            this.btnGetToken.TabIndex = 3;
+            this.btnGetToken.Text = "Get Token";
+            this.btnGetToken.UseVisualStyleBackColor = true;
+            this.btnGetToken.Click += new System.EventHandler(this.btnGetToken_Click);
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(53, 31);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(278, 20);
+            this.txtCommand.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 465);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.btnGetToken);
+            this.Controls.Add(this.btnSendToEmulator);
+            this.Controls.Add(this.btnSendToHost);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Native API Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtResult;
+        private System.Windows.Forms.Button btnSendToHost;
+        private System.Windows.Forms.Button btnSendToEmulator;
+        private System.Windows.Forms.Button btnGetToken;
+        private System.Windows.Forms.TextBox txtCommand;
+    }
+}
+
---
+++ NativeApi/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ; 
+
+namespace NativeApi
+{
+    public partial class Form1 : Form
+    {
+
+        // url for local broker 
+        static string URL = "tcp://localhost:61616";
+
+        // red app id for the plugin running in SRW 
+
+        static string redAppID = "example-native-7lkkd5c";
+
+        // this will be used to send messages to be broker 
+        static IMessageProducer producer;
+
+        // need these objects available in all methods 
+        static ISession session;
+        static IDestination destinationRequest;
+        static IDestination destinationResponse;
+        static IMessageConsumer consumer; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            initializeBrokerConnection();
+            SubscribeToEvents();
+            SubscribeToSyncEvents();
+
+
+        }
+
+        private void initializeBrokerConnection()
+
+        {
+
+            string[] temp = WindowsIdentity.GetCurrent().Name.Split('\\');
+
+            String userDomain = temp[0];
+            String userName = temp[1];
+
+            //  IConnectionFactory connection = 
+            NMSConnectionFactory factory = new NMSConnectionFactory(URL);
+
+            string RequestqueueName = redAppID + "_" + userDomain + "\\" + userName + "_request";
+            string ResponsequeueName = redAppID + "_" + userDomain + "\\" + userName + "_response";
+
+            IConnection con = factory.CreateConnection();
+
+
+            con.Start();
+
+            txtResult.Text = "Connection Started !!! ";
+
+            session = con.CreateSession(AcknowledgementMode.AutoAcknowledge);
+
+            destinationRequest = session.GetQueue(RequestqueueName);
+            destinationResponse = session.GetQueue(ResponsequeueName);
+           
+
+            // get the resposne and show it in the text box 
+          //  consumer = session.CreateConsumer(destinationRequest);
+
+            producer = session.CreateProducer(destinationRequest);
+            producer.DeliveryMode = MsgDeliveryMode.NonPersistent;
+
+            IMessageConsumer responseConsumer = session.CreateConsumer(destinationResponse);
+            txtResult.Text = ""; 
+
+            responseConsumer.Listener += (message) =>
+            {
+
+                // To Access GUI from a different thread 
+
+                Invoke(new Action(() =>
+                {
+                    ITextMessage txtMsg = message as ITextMessage;
+                    string body = txtMsg.Text;
+
+                   //txtResult.Text = body; 
+                    ParseXML(body); 
+                }));
+     
+
+            };
+
+        }
+
+
+
+        private void btnSendToHost_Click(object sender, EventArgs e)
+        {
+            // send command to the Host 
+            string command = txtCommand.Text.ToUpper();
+            String textMsg = "<?xml version='1.0' encoding='UTF-8'?><ns1:SendHostCommandRQ xmlns:ns1='http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0'  >" +
+"<Command>" + command + "</Command>" +
+"</ns1:SendHostCommandRQ>";
+
+            SendMessage(textMsg);
+
+        }
+
+        private void btnSendToEmulator_Click(object sender, EventArgs e)
+        {
+            // send command to the emulator 
+            string command = txtCommand.Text.ToUpper();
+
+            String Textmsg = "<?xml version='1.0' encoding='UTF-8'?><ns1:ExecuteInEmuRQ xmlns:ns1='http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0' showCommand='true' showResponse='true'>" +
+"<Command>" + command + "</Command>" +
+"</ns1:ExecuteInEmuRQ>";
+
+
+            SendMessage(Textmsg); 
+
+
+        }
+
+        private void btnGetToken_Click(object sender, EventArgs e)
+        {
+            // get the session Token 
+
+            String Textmsg = "<?xml version='1.0' encoding='UTF-8'?>" +
+"<ns1:GetSessionSecurityTokenRQ xmlns:ns1='http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0'/>";
+
+            SendMessage(Textmsg);
+
+
+        }
+
+        private void SendMessage(string Textmsg)
+        {
+
+            // send the message 
+            producer.Send(session.CreateTextMessage(Textmsg));
+
+ 
+        }
+
+        // will susbcribe to the commands declared in the redapp.xml 
+        private void SubscribeToEvents()
+        {
+            String Textmsg = "<?xml version='1.0' encoding='UTF-8'?>" +
+"<ns1:EventSubscriptionRQ xmlns:ns1='http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0'/>";
+
+            SendMessage(Textmsg); 
+
+        }
+
+        private void SubscribeToSyncEvents()
+        {
+
+            String Textmsg = "<?xml version='1.0' encoding='UTF-8'?>" +
+"<ns1:CommandSubscriptionRQ xmlns:ns1='http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0'/>";
+
+            SendMessage(Textmsg); 
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xmlText"></param>
+        private void ParseXML(string xmlText)
+        {
+            txtResult.Text = ""; 
+            
+            var Doc =  XDocument.Parse(xmlText);
+
+          //  txtResult.Text = Doc.Root.Name.LocalName;
+            var Hostrsp = from response in Doc.Descendants("Response") select response;
+
+            foreach(var r in Hostrsp)
+            {
+                txtResult.Text += r.Value;
+
+            }
+
+            var Othrsp = from response in Doc.Descendants("ath") select response; 
+            
+            foreach(var oth in Othrsp)
+            {
+                txtResult.Text += oth.Value; 
+
+            }
+
+         
+            if (Doc.Root.Name.LocalName.Equals("EventSubscriptionRS"))
+            {
+                txtResult.Text = "Command Intercepted ";
+            }
+
+            if (Doc.Root.Name.LocalName.Equals("CommandInterceptionRQ"))
+            {
+
+                // addtional method to intercept the command or let it pass 
+
+                string command = Doc.Root.Attribute("command").Value;
+                interceptCommand(command);
+            }
+         
+        
+
+        }
+
+
+        private void interceptCommand(string command)
+        {
+            string textmg; 
+            DialogResult dialogResult = MessageBox.Show("Do you want to Intercept the Command ? ", " ", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                
+           textmg  = "<?xml version='1.0' encoding='UTF-8'?>" +
+       "<com.sabre.edge.dynamo.nativeapi:CommandInterceptionRS xmlns:com.sabre.edge.dynamo.nativeapi='http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0' command='' />";
+
+                SendMessage(textmg); 
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+               textmg = "<?xml version='1.0' encoding='UTF-8'?>" +
+"<com.sabre.edge.dynamo.nativeapi:CommandInterceptionRS xmlns:com.sabre.edge.dynamo.nativeapi='http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0' command='"+command+"' />";
+                SendMessage(textmg);
+            }
+        }
+
+    }
+}
